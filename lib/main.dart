054f4661877f8f83059c6c34bec0e5da83a85e27
haru <godjaewook@naver.com>
2024-05-16 00:06:54 +0900

    6.2장까지

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:haruflix/screens/home_screens.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,8 +11,8 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Container(),
+    return const MaterialApp(
+      home: HomeScreen(),
     );
   }
 }
