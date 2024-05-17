d09a4a10dd3c0b36f58fdd0dbcb9fc80afc1b405
haru <88703915+lips85@users.noreply.github.com>
2024-05-17 16:11:17 +0900

    회사 문제 (인증서) + API 합본

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,21 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:haruflix/screens/home_screens.dart';
+import 'package:haruflix/services/api_service.dart';
 
 void main() {
+  HttpOverrides.global = MyHttpOverrides();
+  ApiService().getTodaysToon();
   runApp(const MyApp());
+}
+
+class MyHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..badCertificateCallback =
+          (X509Certificate cert, String host, int port) => true;
+  }
 }
 
 class MyApp extends StatelessWidget {
---
+++ lib/services/api_service.dart
@@ -0,0 +1,24 @@
+import 'dart:convert';
+
+import 'package:haruflix/models/webtoon_model.dart';
+import 'package:http/http.dart' as http;
+
+class ApiService {
+  final String baseUrl = 'https://webtoon-crawler.nomadcoders.workers.dev';
+  final String today = "today";
+
+  Future<List<WebtoonModel>> getTodaysToon() async {
+    List<WebtoonModel> webtoonInstances = [];
+    final url = Uri.parse("$baseUrl/$today");
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      final List<dynamic> webtoons = jsonDecode(response.body);
+      for (var webtoon in webtoons) {
+        webtoonInstances.add(WebtoonModel.fromJson(webtoon));
+      }
+      return webtoonInstances;
+    } else {
+      throw Exception('Failed to load data');
+    }
+  }
+}
